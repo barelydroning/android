306854449c6477664488fb3621cf25e0dec9a571
elroi92 <94e528f7@opayq.com>
2017-11-16 00:32:12 +0100

    Fixed PID: There was a mix-up between two parameters. Improved some functionality.

--- app/src/main/java/com/barelydroning/drone/PID.java
+++ app/src/main/java/com/barelydroning/drone/PID.java
@@ -16,7 +16,6 @@ public class PID {
     // Variables
     //
     private double integral;
-    private double lastSensorSignal;
     private double lastTargetValue;
     private double lastError;
     private long lastCallTime = 0;
@@ -29,7 +28,7 @@ public class PID {
     public PID(double kp, double ki, double kd) {
         setParameters(kp,ki,kd);
         setOutputLimits(-1000, 1000);
-        setIntegralLimit((outputLimits[1] - outputLimits[0])/kd);
+        setIntegralLimit((outputLimits[1] - outputLimits[0])/ki);
     }
 
     public void setParameters(double kp, double ki, double kd) {
@@ -50,30 +49,34 @@ public class PID {
 
     public double calculate(double targetValue, double filteredSensorValue) {
         long newTime = System.nanoTime();
-
-        if (lastCallTime == 0) {
-            lastCallTime = newTime;
-            lastSensorSignal = filteredSensorValue;
-            lastTargetValue = targetValue;
-            lastError = 0;
-            integral = 0;
-
-            return 0;
-        }
-
         double deltaTime = (newTime - lastCallTime) / 1e9;
-        deltaTime = 1;
         double error = targetValue - filteredSensorValue;
 
+        deltaTime = 1;
+
         // Integral
-        integral += 0.5 * (error - lastError) * deltaTime;
+        integral += 0.5 * (error + lastError) * deltaTime;
         if (integral > maxIntegral) integral = maxIntegral;
+        if (integral < -maxIntegral) integral = -maxIntegral;
         //if (lastTargetValue != targetValue) integral = 0;
 
         // Derivative
         double derivative = (lastTargetValue == targetValue) ? (error - lastError)/deltaTime : 0;
 
-        return parameters[0]*error + parameters[1]*integral + parameters[2]*derivative;
-    }
+        // No point in increasing the derivative or the integral if this is the first call
+        if (lastCallTime == 0) {
+            derivative = 0;
+            integral = 0;
+        }
 
+        // Output
+        double output =  parameters[0]*error + parameters[1]*integral + parameters[2]*derivative;
+
+        // Save required values for next run
+        lastError = error;
+        lastCallTime = newTime;
+        lastTargetValue = targetValue;
+
+        return constrain(output);
+    }
 }
